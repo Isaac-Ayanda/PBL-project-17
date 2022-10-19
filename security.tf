6348aa4697f6da49ccd98c14efe315ffd59fa3ec
Isaac Ayanda <isaacayanda@gmail.com>
2022-10-19 16:35:47 +0100

    Update security.tf

--- security.tf
+++ security.tf
@@ -36,6 +36,15 @@ resource "aws_security_group" "ext-alb-sg" {
 
 }
 
+# Add rule to allow ssh access from bastion_sg
+resource "aws_security_group_rule" "inbound_ext_alb_ssh" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.bastion_sg.id
+  security_group_id        = aws_security_group.ext_alb_sg.id
+}
 
 # security group for bastion, to allow access into the bastion host from you IP
 resource "aws_security_group" "bastion_sg" {
